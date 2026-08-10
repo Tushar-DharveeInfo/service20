5bb631359d15ca4161c8ffa7604b9b47854ee33a
Tushar Joshi <tushar.joshi@dharveeinfotech.com>
2026-08-10 18:56:47 +0530

    removed unused icon

--- src/component/shared/allcommon/menu/FnGetIconForExpandableMenu.ts
+++ src/component/shared/allcommon/menu/FnGetIconForExpandableMenu.ts
@@ -2,15 +2,8 @@
 import { FnResolveIcons, N } from "./FnResolveIcons";
 
 const expandableAliases = {
-    rackandstack24x24: "rackandstack24x24",
-    workorder24x24: "workorder24x24",
     settings24x24: "setting24x24",
-    assetassignment24x24: "assetassigment24x24",
-    changemanagement24x24: "change24x24",
-    selectdatacentersite24x24: "alldatacenters24x24",
-    rowofracks24x24: "rowofracks24x24",
-    applicationsettings24x24: "setting24x24",
-    myactivities24x24: 'MyActvities24x24'
+    myactivities24x24: "MyActvities24x24",
 };
 
 const FnGetIconForExpandableMenu = FnResolveIcons({
